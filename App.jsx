e84d440587f1e3aa88d381443c192866f36a0126
Patrick Vallon <jpatrickvallon@gmail.com>
2023-07-02 22:32:01 +0800

    Remove header

--- App.jsx
+++ App.jsx
@@ -10,7 +10,10 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
