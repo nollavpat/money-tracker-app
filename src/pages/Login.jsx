7cc9ce7f3a51b743475f6492290a262878b1e448
Patrick Vallon <jpatrickvallon@gmail.com>
2023-07-16 15:15:54 +0800

    Convert to tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import ReactNativeBiometrics from 'react-native-biometrics';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {useAtom} from 'jotai';
@@ -30,19 +30,25 @@ const Login = ({navigation}) => {
   };
 
   return (
-    <View style={styles.screenContainer}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>MT</Text>
+    <View className="p-6">
+      <View>
+        <Text className="text-3xl font-medium text-green-500">MT</Text>
       </View>
-      <View style={styles.greetingsContainer}>
-        <Text style={styles.greetings}>Hello Patrick,</Text>
-        <View style={styles.subGreetingsContainer}>
-          <Text style={styles.subGreeting}>Fucking save man, don't</Text>
-          <Text style={styles.subGreeting}>be an idiot.</Text>
+      <View className="mt-40">
+        <Text className="text-3xl font-bold text-neutral-950">
+          Hello Patrick,
+        </Text>
+        <View className="mx-6">
+          <Text className="text-xl font-normal text-neutral-500">
+            Fucking save man, don't
+          </Text>
+          <Text className="text-xl font-normal text-neutral-500">
+            be an idiot.
+          </Text>
         </View>
       </View>
-      <View style={styles.fingerprintContainer}>
-        <View style={styles.fingerprintButtonContainer}>
+      <View className="mt-52 items-center justify-center">
+        <View className="h-20 w-20 items-center justify-center rounded-full bg-green-500">
           <Pressable onPress={onFingerprint}>
             <MaterialIcons name="fingerprint" size={64} color="#FAFAFA" />
           </Pressable>
@@ -51,58 +57,5 @@ const Login = ({navigation}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    backgroundColor: '#FAFAFA',
-    height: '100%',
-    padding: 16,
-  },
-  titleContainer: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-  },
-  title: {
-    color: '#22C55E',
-    fontFamily: 'Roboto',
-    fontSize: 36,
-    fontStyle: 'normal',
-    fontWeight: '500',
-  },
-  greetingsContainer: {
-    marginTop: 160,
-    paddingHorizontal: 16,
-  },
-  greetings: {
-    color: '#000',
-    fontFamily: 'Roboto',
-    fontSize: 36,
-    fontStyle: 'normal',
-    fontWeight: '700',
-  },
-  subGreetingsContainer: {
-    paddingHorizontal: 18,
-  },
-  subGreeting: {
-    color: '#737373',
-    fontFamily: 'Roboto',
-    fontSize: 20,
-    fontStyle: 'normal',
-    fontWeight: '500',
-  },
-  fingerprintContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 200,
-  },
-  fingerprintButtonContainer: {
-    alignItems: 'center',
-    backgroundColor: '#22C55E',
-    borderRadius: 42,
-    height: 84,
-    justifyContent: 'center',
-    width: 84,
-  },
-});
 
 export default Login;
