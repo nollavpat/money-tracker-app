ea6931d694967cbc3700101092914b344ab2fcc4
Patrick Vallon <jpatrickvallon@gmail.com>
2023-07-23 16:55:19 +0800

    Fix loading

--- src/pages/Home/Body/Body.jsx
+++ src/pages/Home/Body/Body.jsx
@@ -1,18 +1,24 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, View} from 'react-native';
+import React, {useEffect, useMemo, useState} from 'react';
+import {ActivityIndicator, FlatList, View} from 'react-native';
 import dayjs from 'dayjs';
 
 import TransactionsByDate from './TransactionsByDate';
+
 import TransactionWebservice from '../../../webservices/money_tracker/TransactionWebservice';
 
 const DATE_FORMAT = 'MMMM DD, dddd';
 
 const Body = () => {
   const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
+
       const [error, data] = await TransactionWebservice.getTransactions();
+
+      setIsLoading(false);
 
       if (!error) {
         setTransactions(data);
@@ -20,27 +26,37 @@ const Body = () => {
     })();
   }, []);
 
-  const transactionsGroupByDate = transactions.reduce((acc, curr) => {
-    const date = dayjs(curr.created_at).format(DATE_FORMAT);
+  const transactionsGroupByDate = useMemo(() => {
+    return transactions.reduce((acc, curr) => {
+      const date = dayjs(curr.created_at).format(DATE_FORMAT);
 
-    return {
-      ...acc,
-      [date]: [...(acc[date] || []), curr],
-    };
-  }, {});
+      return {
+        ...acc,
+        [date]: [...(acc[date] || []), curr],
+      };
+    }, {});
+  }, [transactions]);
 
   return (
-    <View className="items-center bg-neutral-100">
-      <FlatList
-        keyExtractor={transactionDate => transactionDate}
-        data={Object.keys(transactionsGroupByDate)}
-        renderItem={({item: transactionDate}) => (
-          <TransactionsByDate
-            transactionDate={transactionDate}
-            transactions={transactionsGroupByDate[transactionDate]}
-          />
-        )}
-      />
+    <View className="flex-grow items-center justify-center bg-neutral-100">
+      {(() => {
+        if (isLoading) {
+          return <ActivityIndicator size="large" color="#22c55e" />;
+        } else {
+          return (
+            <FlatList
+              keyExtractor={transactionDate => transactionDate}
+              data={Object.keys(transactionsGroupByDate)}
+              renderItem={({item: transactionDate}) => (
+                <TransactionsByDate
+                  transactionDate={transactionDate}
+                  transactions={transactionsGroupByDate[transactionDate]}
+                />
+              )}
+            />
+          );
+        }
+      })()}
     </View>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {View} from 'react-native';
 
 import Body from './Body/Body';
 import Header from './Header';
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <View className="flex flex-col">
-        <Header />
-        <Body />
-      </View>
-    </SafeAreaView>
+    <View className="flex-1">
+      <Header />
+      <Body />
+    </View>
   );
 };
 
